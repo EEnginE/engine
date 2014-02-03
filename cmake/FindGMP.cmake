c749f4de90f1b205d58eb40aa43d8a973dfc2c6f
mensinda <mensinda42@gmail.com>
2014-02-03 13:29:02 +0100

    CMake: GMP_PREFIX --> GMP_ROOT; + EclipseFormater.xml

--- cmake/FindGMP.cmake
+++ cmake/FindGMP.cmake
@@ -1,4 +1,7 @@
 # Try to find the GMP librairies
+#
+# GMP_ROOT        - The root install path of the GMP lib
+#
 # GMP_FOUND       - system has GMP lib
 # GMP_INCLUDE_DIR - the GMP include directory
 # GMP_LIBRARIES   - Libraries needed to use GMP
@@ -8,9 +11,9 @@ if (GMP_INCLUDE_DIR AND GMP_LIBRARIES)
                 set(GMP_FIND_QUIETLY TRUE)
 endif (GMP_INCLUDE_DIR AND GMP_LIBRARIES)
 
-find_path(GMP_INCLUDE_DIR NAMES gmp.h PATHS ${GMP_PREFIX}/include /usr/include /usr/local/include )
-find_library(GMP_LIBRARIES NAMES gmp libgmp PATHS ${GMP_PREFIX}/lib /usr/lib /usr/local/lib )
-find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx PATHS ${GMP_PREFIX}/lib /usr/lib /usr/local/lib )
+find_path(GMP_INCLUDE_DIR NAMES gmp.h PATHS ${GMP_ROOT}/include /usr/include /usr/local/include )
+find_library(GMP_LIBRARIES NAMES gmp libgmp PATHS ${GMP_ROOT}/lib /usr/lib /usr/local/lib )
+find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx PATHS ${GMP_ROOT}/lib /usr/lib /usr/local/lib )
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARIES)
