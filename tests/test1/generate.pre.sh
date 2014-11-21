a44b56f4b8e1b79d723f4dbb1b4ae8bbc4beb371
Arvius <arvius@web.de>
2014-11-21 20:52:36 +0100

    Improved generate mechanism. Clean process now working. Automatic objParser is now flawless working.

--- tests/test1/generate.pre.sh
+++ tests/test1/generate.pre.sh
@@ -2,14 +2,22 @@
 OBJFILES=./data/*.obj
 
 if [ ! -x ../oglTest/oglTestBindings.sh ]; then
-   echo "ERROR:      -- Unable to find ../oglTest/oglTestBindings.sh"
+   echo "ERROR:     -- Unable to find ../oglTest/oglTestBindings.sh"
    exit 2
+fi
+
+
+TOOLSPATH=$ENGINE_ROOT
+if [ -z "$ENGINE_ROOT" ]; then
+   echo "WARNING:   -- ENGINE_ROOT not set. Fallback to relative paths."
+   TOOLSPATH="../.."
+   export ENGINE_ROOT=$(readlink -m ../../)
 fi
 
 for f in $OBJFILES
 do
    echo "INFO:      -- Processing on $f"
-   ../../tools/objParse.awk $f
+   ${TOOLSPATH}/tools/objParse.awk $(readlink -m $f)
 done
 
 echo "INFO:      -- Generating oglTestBind.hpp and oglTestBind.cpp"
