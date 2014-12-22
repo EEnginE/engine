6cf14ea062800b489d67e00e9fe7d2855114599d
mensinda <mensinda42@gmail.com>
2014-12-22 15:10:40 +0100

    Fixed commandline argument regex

--- tests/oglTest/cmdANDinit.cpp
+++ tests/oglTest/cmdANDinit.cpp
@@ -11,7 +11,7 @@ cmdANDinit::cmdANDinit( int argc, char *argv[], testStarter &_starter, bool &_er
    dataRoot     = INSTALL_PREFIX + string( "/share/engineTests/oglTest/data/" );
 #endif
 
-   for ( auto i = 1; i < argc; ++i ) {
+   for( auto i = 1; i < argc; ++i ) {
       args.push_back( argv[i] );
    }
 
@@ -32,7 +32,7 @@ cmdANDinit::cmdANDinit( int argc, char *argv[], testStarter &_starter, bool &_er
    GlobConf.versions.glMinorVersion = 6;
 
 
-   if ( vCanUseColor ) {
+   if( vCanUseColor ) {
       GlobConf.log.logOUT.colors   = FULL;
       GlobConf.log.logERR.colors   = FULL;
    } else {
@@ -55,7 +55,7 @@ cmdANDinit::cmdANDinit( int argc, char *argv[], testStarter &_starter, bool &_er
 #endif
 
    GlobConf.log.waitUntilLogEntryPrinted = false;
-   
+
    _errors = !parseArgsAndInit( _starter );
 
 }
@@ -63,116 +63,116 @@ cmdANDinit::cmdANDinit( int argc, char *argv[], testStarter &_starter, bool &_er
 
 
 void cmdANDinit::usage() {
-   iLOG( "Usage: ", argv0, " [OPTIONS] OUTPUT_FILES"  );
+   iLOG( "Usage: ", argv0, " [OPTIONS] OUTPUT_FILES" );
    iLOG( "" );
    iLOG( "OPTIONS:" );
-   dLOG( "    -h | --help      : show this help message"                  );
-   dLOG( "    --log=<path>     : set a custom log file path to <path>"    );
-   dLOG( "    -w | --wait      : wait until log entry is printed"         );
+   dLOG( "    -h | --help      : show this help message" );
+   dLOG( "    --log=<path>     : set a custom log file path to <path>" );
+   dLOG( "    -w | --wait      : wait until log entry is printed" );
    dLOG( "    --data=<path>    : set a custom root path for the data dir" );
    dLOG( "    --glMajor=<v>    : the OpenGL Major version (default: "
-  ,  GlobConf.versions.glMajorVersion, ")"  );
+         ,  GlobConf.versions.glMajorVersion, ")" );
    dLOG( "    --glMinor=<v>    : the OpenGL Major version (default: "
-  ,  GlobConf.versions.glMinorVersion, ")"  );
-   if ( vCanUseColor ) {
-      dLOG( "    -n | --nocolor   : disable colored output"                  );
+         ,  GlobConf.versions.glMinorVersion, ")" );
+   if( vCanUseColor ) {
+      dLOG( "    -n | --nocolor   : disable colored output" );
    }
    dLOG( "    -l | --list      : lists all available tests" );
-   dLOG( "    --without-<test> : disables test <test>"      );
-   dLOG( "    --with-<test>    : enables test <test>"       );
-   dLOG( "    --all-off        : disables all test"         );
-   dLOG( "    --all-on         : enables all test"          );
+   dLOG( "    --without-<test> : disables test <test>" );
+   dLOG( "    --with-<test>    : enables test <test>" );
+   dLOG( "    --all-off        : disables all test" );
+   dLOG( "    --all-on         : enables all test" );
 }
 
 
 bool cmdANDinit::parseArgsAndInit( testStarter &_starter ) {
-   for ( auto const & arg : args ) {
-      if ( arg == "-h" || arg == "--help" ) {
+   for( auto const & arg : args ) {
+      if( arg == "-h" || arg == "--help" ) {
          usage();
          return false;
       }
 
-      if ( arg == "-w" || arg == "--wait" ) {
+      if( arg == "-w" || arg == "--wait" ) {
          iLOG( "Wait is enabled" );
          GlobConf.log.waitUntilLogEntryPrinted = true;
          continue;
       }
 
-      if ( ( arg == "-n" || arg == "--nocolor" ) && vCanUseColor ) {
+      if( ( arg == "-n" || arg == "--nocolor" ) && vCanUseColor ) {
          iLOG( "Color is disabled" );
          GlobConf.log.logOUT.colors   = DISABLED;
          GlobConf.log.logERR.colors   = DISABLED;
          continue;
       }
 
-      std::regex lLogRegex( "^\\-\\-log=[\\/a-zA-Z0-9 \\._\\-\\+\\*]+$" );
-      if ( std::regex_match( arg, lLogRegex ) ) {
+      std::regex lLogRegex( "^\\-\\-log=[a-zA-Z_0-9 \\/\\.\\-\\+\\*]+$" );
+      if( std::regex_match( arg, lLogRegex ) ) {
          std::regex lLogRegexRep( "^\\-\\-log=" );
          const char *lRep = "";
          string logPath = std::regex_replace( arg, lLogRegexRep, lRep );
          GlobConf.log.logFILE.logFileName = logPath;
          continue;
       }
-      
-      std::regex lDataRegex( "^\\-\\-data=[\\/a-zA-Z0-9 \\._\\-\\+\\*]+$" );
-      if ( std::regex_match( arg, lDataRegex ) ) {
+
+      std::regex lDataRegex( "^\\-\\-data=[a-zA-Z_0-9 \\/\\.\\-\\+\\*]+$" );
+      if( std::regex_match( arg, lDataRegex ) ) {
          std::regex lDataRegexRep( "^\\-\\-data=" );
          const char *lRep = "";
          dataRoot = std::regex_replace( arg, lDataRegexRep, lRep );
          continue;
       }
-      
+
       std::regex lMajorRegex( "^\\-\\-glMajor=[0-9]+$" );
-      if ( std::regex_match( arg, lMajorRegex ) ) {
+      if( std::regex_match( arg, lMajorRegex ) ) {
          std::regex lDataRegexRep( "^\\-\\-glMajor=" );
          const char *lRep = "";
          string version = std::regex_replace( arg, lDataRegexRep, lRep );
          GlobConf.versions.glMajorVersion = atoi( version.c_str() );
          continue;
       }
-      
+
       std::regex lMinorRegex( "^\\-\\-glMinor=[0-9]+$" );
-      if ( std::regex_match( arg, lMinorRegex ) ) {
+      if( std::regex_match( arg, lMinorRegex ) ) {
          std::regex lDataRegexRep( "^\\-\\-glMinor=" );
          const char *lRep = "";
          string version = std::regex_replace( arg, lDataRegexRep, lRep );
          GlobConf.versions.glMinorVersion = atoi( version.c_str() );
          continue;
       }
-      
-      
-      if ( arg == "--list" || arg == "-l" ) {
+
+
+      if( arg == "--list" || arg == "-l" ) {
          _starter.list();
          return false;
       }
-      
-      
-      std::regex lWithoutRegex( "^\\-\\-without\\-[\\/a-zA-Z0-9 \\._\\-\\+\\*]+$" );
-      if ( std::regex_match( arg, lWithoutRegex ) ) {
+
+
+      std::regex lWithoutRegex( "^\\-\\-without\\-[a-zA-Z_0-9 \\/\\.\\-\\+\\*]+$" );
+      if( std::regex_match( arg, lWithoutRegex ) ) {
          std::regex lDataRegexRep( "^\\-\\-without\\-" );
          const char *lRep = "";
          _starter.disable( std::regex_replace( arg, lDataRegexRep, lRep ) );
          continue;
       }
-      
-      std::regex lWithRegex( "^\\-\\-with\\-[\\/a-zA-Z0-9 \\._\\-\\+\\*]+$" );
-      if ( std::regex_match( arg, lWithRegex ) ) {
+
+      std::regex lWithRegex( "^\\-\\-with\\-[a-zA-Z_0-9 \\/\\.\\-\\+\\*]+$" );
+      if( std::regex_match( arg, lWithRegex ) ) {
          std::regex lDataRegexRep( "^\\-\\-with\\-" );
          const char *lRep = "";
          _starter.enable( std::regex_replace( arg, lDataRegexRep, lRep ) );
          continue;
       }
-      
-      if ( arg == "--all-off" ) {
+
+      if( arg == "--all-off" ) {
          _starter.allTestsOff();
          continue;
       }
-      
-      if ( arg == "--all-on"  ) {
+
+      if( arg == "--all-on" ) {
          _starter.allTestsOn();
          continue;
       }
-      
+
       outputFiles.push_back( arg );
    }
 
@@ -184,7 +184,7 @@ void cmdANDinit::generate( uJSON_data &_data ) {
    if( outputFiles.empty() ) {
       outputFiles.emplace_back( "./oglTest.json" );
    }
-   for( auto &f : outputFiles ) {
+   for( auto & f : outputFiles ) {
       uParserJSON generator( f );
       if( generator.write( _data, true ) == 1 ) {
          iLOG( "Successfully written '", f, "'" );
@@ -196,4 +196,4 @@ void cmdANDinit::generate( uJSON_data &_data ) {
 
 
 
-// kate: indent-mode cstyle; indent-width 3; replace-tabs on; line-numbers on; remove-trailing-spaces on;
+// kate: indent-mode cstyle; indent-width 3; replace-tabs on; line-numbers on;remove-trailing-spaces on;
